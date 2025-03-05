cd5e94420740644a9e2c03dc479d37166f638648
HaeSoo <gumake999@gmail.com>
2025-03-05 19:06:31 +0900

    FEAT: list 조회 api로 요청하도록 하기 및 캐싱 추가

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -1,22 +1,13 @@
-import { connectDB } from "@/util/database";
-
 import ListItem from "./ListItem";
 
-export const dynamic = 'force-dynamic'; // 'force-static'
-
 export default async function List() {
-  const db = (await connectDB).db('forum'); // collections 이름
-  const results = await db.collection('post').find().toArray(); // document 출력
-
-  console.log('results0', results)
+  // 요청 url 별 캐싱
+  const res = await fetch("http://localhost:3000/api/postGet", { method: 'GET', next: { revalidate: 10 }});
+  const { posts } = await res.json();
 
   return (
     <div className="list-bg">
-      <ListItem results={JSON.stringify(results)} />
-      {/* results가 배열이므로 원래는 그냥 props로 넘길 수 있지만,
-      _id가 ObjectId('..')로 되어서인지 정상적으로 넘어가지 않는다.
-      ( _id: {buffer: ...} <- 이런 식으로 오류. )
-      따라서 JSON.stringify()로 배열을 문자열화 해서 props로 넘긴 후 다시 배열화 시키는 방법 사용 */}
+      <ListItem results={posts} />
     </div>
   )
 }
